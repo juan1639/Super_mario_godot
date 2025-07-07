ebbc143a62b3481b4525c9a121900c4c94ee1fad
unknown <juaneguiabad1639@gmail.com>
2025-07-08 00:21:38 +0200

    Agregada conexion entre escenas (main-menu y juego)

---
+++ scripts/main_menu.gd
@@ -0,0 +1,9 @@
+extends PanelContainer
+
+@onready var musicaIntro = get_node("../MusicaIntroMarioSnes")
+
+func _ready():
+	musicaIntro.play()
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
